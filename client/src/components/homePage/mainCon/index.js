8c0a67996298e45210ab727ba9913b6b97d15d4d
clueking1 <wcoan300@gmail.com>
2020-06-04 11:34:54 -0400

    created automation of gradient upon main page load

--- client/src/components/homePage/mainCon/index.js
+++ client/src/components/homePage/mainCon/index.js
@@ -6,15 +6,15 @@ function HomePageCon() {
     return (
         <div className="homePageCon">
             <h2 className="homeTitle">
-                <span className="homeTitleRe">Re</span>
-                <span className="homeTitleCom">COMMERCE</span>
+                <span className="homeTitleRe a">Re</span>
+                <span className="homeTitleCom a">COMMERCE</span>
             </h2>
             <h2 className="homeRev">
-                <span className="homeRevRe">Re</span>
-                <span className="homeRevCom">MEMBER</span>
-                <span className="homeIn">TO</span>
-                <span className="homeSelRe">Re</span>
-                <span className="homeSelCom">SELL</span>
+                <span className="homeRevRe a">Re</span>
+                <span className="homeRevCom a">MEMBER</span>
+                <span className="homeIn a">TO</span>
+                <span className="homeSelRe a">Re</span>
+                <span className="homeSelCom a">SELL</span>
             </h2>
         </div>
     )
--- client/src/components/homePage/mainPage/index.js
+++ client/src/components/homePage/mainPage/index.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
 function HomePage() {
+    const [backColor, setBackColor] = useState()
+
+    useEffect(() => {
+        let degree = -90
+        let color = 1 
+        let color1 = 1
+        let color2 = 1
+        setBackColor({backgroundImage : `-webkit-linear-gradient(90deg, rgb(15, 15, 15), rgb(22, 59, 90))`})
+        let int = setInterval(function (){ 
+            if (degree !== 90) {
+                degree++
+                color++
+                color1++
+                color2++
+            setBackColor({backgroundImage : `-webkit-linear-gradient(${degree}deg, rgb(15, 15, 15), rgb(${color1 / 8.22}, ${color2 / 3.06}, ${color / 2}))`})
+            } 
+            console.log('hi')
+            if (degree === 90) {
+                clearInterval(int)
+             }
+         }, 10)
+    }, [])
 
     return (
         <div className="homePageWrapper">
-            <div className="homePageColor">
+            <div className="homePageColor" style={backColor}>
                 
             </div>
         </div>
