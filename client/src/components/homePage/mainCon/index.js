67e5fca584535374ff690fc3c4babad40a320f03
clueking1 <wcoan300@gmail.com>
2020-06-05 19:33:32 -0400

    added opacity automation to slogan

--- client/src/components/homePage/mainCon/index.js
+++ client/src/components/homePage/mainCon/index.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 
 function HomePageCon() {
+
+    const [op, setOp] = useState()
+
+    useEffect(() => {
+        let color = 1 
+        let start = 0
+        setOp({opacity : "0"})
+        let int = setInterval(function (){ 
+            if (color !== 0) {
+     
+                color++
+                start += color / 181
+            //setOp({opacity : `${start / 181}`, transform: `scale(${start / 181})`, webkitTransform:  `scale(${start / 181})`})
+            setOp({opacity : `${start / 181}`})
+            } 
+            console.log(op)
+            if (op > 1) {
+                clearInterval(int)
+             }
+         }, 7)
+       setInterval(function (){ 
+            clearInterval(int)
+        }, 1800)
+         
+    }, [])
 
     return (
         <div className="homePageCon">
@@ -9,7 +34,7 @@ function HomePageCon() {
                 <span className="homeTitleRe a">Re</span>
                 <span className="homeTitleCom a">COMMERCE</span>
             </h2>
-            <h2 className="homeRev">
+            <h2 className="homeRev" style = {op}>
                 <span className="homeRevRe a">Re</span>
                 <span className="homeRevCom a">MEMBER</span>
                 <span className="homeIn a">TO</span>
