12aa10590821c84ece7d04e9bc7b14395a5ffa65
clueking1 <wcoan300@gmail.com>
2020-06-15 10:19:37 -0400

    checkout product line info is all lined up

--- client/src/components/checkOutPage/checkOutContent/index.js
+++ client/src/components/checkOutPage/checkOutContent/index.js
@@ -1,25 +1,48 @@
 import React from 'react'
 import { useAtcContext } from '../../utils/atcStore'
 import './style.css'
+import iphoneWhite from '../../productPage/products/assets/imageIphoneX.jpeg'
+import iphoneBlack from '../../productPage/products/assets/iphoneBlack.jpeg'
+import iphone11 from '../../productPage/products/assets/iphone11.jpg'
 
 function CoContent() {
     const [state] = useAtcContext()
+
+    function image(img) {
+        if (img === "IphoneWhite") {
+            return iphoneWhite
+        } else if (img === "IphoneBlack") {
+            return iphoneBlack
+        } else {
+            return iphone11
+        }
+    }
+
     return( 
         <div className="coContent">
             {state.map(t => (
                 <div className="productInfoWrapper" key={t.src}>
+                    <img className="coImg" src={image(t.src)} />
                     <div className="productInfo">
-                        <p className="productName">{t.item}</p> 
-                        <p className="productQuan">${t.value}</p>
-                        <p className="productQuan">{t.quantity}</p>
-                    </div>
-                    <div className="productPlusSub">
-                        <div className="productPlus">
+                    
+                        <div className="prodNameDiv">
+                            <p className="productNameTit">Item</p> 
+                            <p className="productName">{t.item}</p> 
+                        </div>
+                        <div className="prodValDiv">
+                            <p className="productValueTit">Price</p> 
+                            <p className="productValue">${t.value}</p> 
+                        </div>
+                        <div className="prodPlusSub">
+                            <p className="productQuan">{t.quantity}</p>
+                            <div className="productPlus">
 
+                            </div>
+                            <div className="productSub">
+                                
+                            </div>
                         </div>
-                        <div className="productSub">
-                            
-                        </div>
+                        
                     </div>
                 </div>
             ))}
