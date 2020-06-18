1ecf867ceb39b2adc6bc2b3ed7c1d932c0dac478
clueking1 <wcoan300@gmail.com>
2020-06-18 14:36:22 -0400

    upon successful transaction user is brought back to homepage

--- client/src/components/addOns/paypalBut/index.js
+++ client/src/components/addOns/paypalBut/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import API from '../../utils/API'
 import { useAtcContext } from '../../utils/atcStore'
 import  { useHistory } from 'react-router-dom'
-
 import './style.css'
 import { PayPalButton } from "react-paypal-button-v2";
  
@@ -15,7 +13,11 @@ function PaypalBut(props) {
         amount={props.tot}
         shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
         onSuccess={(details, data) => {
-          alert("Transaction completed by " + details.payer.name.given_name);
+            //details.payer.email_address
+            //send products bought in global state and log it on the back end
+            dispatch({type: "splice"})
+            sessionStorage.clear();
+            history.push("/")
         }}
         options={{
           clientId: "AdrJLpHD26pKdVkc6kHFzH6cCGPKyUb9TJXy9Kg1T57YjcoPU2QKFqpfV6Pma2g1EPdvJ54Zd0_32-Ya"
