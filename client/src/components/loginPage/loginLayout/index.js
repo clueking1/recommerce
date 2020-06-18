2e0063e065f8c82c920ef32f1817bc978df4a92e
clueking1 <wcoan300@gmail.com>
2020-06-18 15:41:09 -0400

    added layout of login page

---
+++ client/src/components/loginPage/loginLayout/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+
+function loginLayout() {
+    return (
+        <div className="loginWrapper">
+            <div className="loginColor">
+                 
+            </div>
+            
+        </div>
+    )
+}
+
+export default loginLayout
--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import Navbar from '../../components/addOns/navbar'
 import Checkout from '../../components/addOns/checkout'
+import LoginLayout from '../../components/loginPage/loginLayout'
 import { useAtcContext } from '../../components/utils/atcStore'
 import { runGlobal } from '../../components/utils/globalStore'
 import './style.css'
@@ -17,7 +18,7 @@ function Login() {
     return (
         <div className="homeWrapper">
             <Navbar />
-
+            <LoginLayout/>
             {state.length >= 1 ? <Checkout /> : undefined} 
         </div>
     )
