dd59dd880ebc1d70ca4eb767ddddd2c28876b943
clueking1 <wcoan300@gmail.com>
2020-06-20 08:59:45 -0400

    added resell to user

--- client/src/components/userPage/userCon/index.js
+++ client/src/components/userPage/userCon/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { useUserContext } from '../../utils/userStore'
 import API from '../../utils/API'
+import './style.css'
 
 
 function UserCon() {
@@ -23,6 +24,21 @@ function UserCon() {
     return (
         <div>
             <button onClick={() => loguserout()}>Log Out</button>
+            <h2 className="uploadHeader">Resell</h2>
+            <div className="uploadDiv">
+                <div className="uploadInputs">
+                    <label className="itemLa">Item: </label>
+                    <input type="text" className="itemIn" />
+                    <label className="itemValLa">$</label>
+                    <input type="text" className="itemVal" />
+                    <label classname="imgLa" for="img">Select image:</label>
+                    <input classname="imgIn" type="file" id="img" name="img" accept="image/*"></input><br />
+                </div>
+                <div className="uploadBut">
+                    <button type="submit" className="itemSub">ReSell Item</button>
+                </div>
+            </div>
+
         </div>
     )
 }
